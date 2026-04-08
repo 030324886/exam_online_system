e08283136edac521621c44070ef1a818bbe839df
“030324886” <2592803243@qq.com>
2026-04-08 20:57:16 +0800

    初始化后端环境

---
+++ backend-server/src/main/java/com/stu/backendserver/BackendServerApplication.java
@@ -0,0 +1,24 @@
+package com.stu.backendserver;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@MapperScan("com.stu.backendserver.mapper")
+public class BackendServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendServerApplication.class, args);
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
